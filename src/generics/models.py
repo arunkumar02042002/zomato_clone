955a9fea34c11322d4789a761d6dc29e91573840
Arun Kumar <arun.kumar.2403gg@gmail.com>
2024-08-20 17:55:55 +0530

    Added Upload and UploadPrivate Models

--- src/generics/models.py
+++ src/generics/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from config.storage_backends import PublicMediaStorage, PrivateMediaStorage
 
-# Create your models here.
+User = get_user_model()
+
+class Upload(models.Model):
+    file = models.FileField(storage=PublicMediaStorage)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.file.url
+
+class UploadPrivate(models.Model):
+    file = models.FileField(storage=PrivateMediaStorage())
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    uploaded_by = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.file.url
+    
